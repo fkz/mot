ba6bf0a07c8a2d0eff04662ad1539969e2eece9d
Sarah Lutteropp <uwcwa@student.kit.edu>
2015-09-15 12:49:30 +0200

    moved mot to center

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ def drawGameOverScreen(screen):
   screen.fill((0,0,0))
   myimage = pygame.image.load("tote_motte.jpg")
   imagerect = myimage.get_rect()
+
+  dx = screen.get_width() / 2 - imagerect.width / 2
+  dy = screen.get_height() / 2 - imagerect.height / 2
+
+  imagerect.move_ip(dx, dy)
+
   screen.blit(myimage, imagerect)
-  myfont = pygame.font.SysFont("monospace", 15)
+  myfont = pygame.font.SysFont("monospace", 25)
   label = myfont.render("All mots are dead!", 1, (255,255,0))
   screen.blit(label, (100, 100))
   pygame.display.update()
 
 if __name__ == "__main__":
   pygame.init()
-  env = Environment(30,30)
-  env.generateRandom(100)
+  env = Environment(10,10)
+  env.generateRandom(40)
   screen = pygame.display.set_mode((800,800))
   env.draw(screen)
   pygame.display.update()
